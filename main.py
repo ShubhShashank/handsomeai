18464a5757f91e545c21b43dc3b543657e74393a
belugaX <handsomebelugawhale@gmail.com>
2017-01-19 18:27:07 +0100

    Did some minor changes and made pmgr file

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import os
-import time
 
-def InitEvent():
-    print "Initializing"
-    init_start_time = time.time()
+def initEvent():
+    print "Initializing..."
     global current_os_dir
     global current_local_dir
     global input_def
     global pkglist
+    global os
+    global pmgr
+    import os
+    import pmgr
     current_os_dir = os.getcwd()
     current_local_dir = "home"
     input_def = "LOCAL://" + current_local_dir + ":"
     pkglist = open('pkg/pkg.list', 'r')
     pkg_found = "null"
-    init_time = time.time() - init_start_time
-    print "Initialized in " + str(pre_init_time)
+    print "Initialized"
 
-preInitEvent()
+initEvent()
 
 while True:
     console_input = raw_input(input_def)
-    if console_input == "install":
+    if "pmgr" in console_input:
         pass
     elif console_input == "ai":
         pass
@@ -28,9 +29,6 @@ while True:
         pkg_found = "null"
         if os.path.isfile('pkg/%s/main.py' % console_input):
             os.system('python pkg/%s/main.py' % console_input)
-            pkg_found = True
-        else:
-            print "Package is not executable"
             pkg_found = True
         if pkg_found != True:
             print "Unknown command or executable package"
